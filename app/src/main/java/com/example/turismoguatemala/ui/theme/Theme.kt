cbcb0480a1ff4e81fa2f5398ba25ede122e27c49
Zacatac23 <zac231104@uvg.edu.gt>
2024-10-02 17:16:21 -0600

    Ajuste de theme

--- app/src/main/java/com/example/turismoguatemala/ui/theme/Theme.kt
+++ app/src/main/java/com/example/turismoguatemala/ui/theme/Theme.kt
@@ -1,58 +1,51 @@
 package com.example.turismoguatemala.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+// Definición de colores
+private val DarkColorPalette = darkColorScheme(
+    primary = Color(0xFF4CAF50), // Verde naturaleza
+    secondary = Color(0xFF2196F3), // Azul cielo
+    background = Color(0xFF1B5E20), // Fondo oscuro
+    surface = Color(0xFFFAFAFA),
     onPrimary = Color.White,
     onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onBackground = Color.White,
+    onSurface = Color.Black
+)
+
+private val LightColorPalette = lightColorScheme(
+    primary = Color(0xFF4CAF50), // Verde naturaleza
+    secondary = Color(0xFF2196F3), // Azul cielo
+    background = Color(0xFFFAFAFA), // Fondo claro
+    surface = Color(0xFFFFFFFF),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onBackground = Color.Black,
+    onSurface = Color.Black
 )
 
 @Composable
 fun TurismoGuatemalaTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = colors,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
